8b56f8e28434ad4e57d9c2b1bc41ebb34bd88e40
Marcos Cabrera Morillas <marcos.cabrera@grupoica.com>
2020-10-10 22:49:18 +0200

    Get files when search asteroid

--- asteroid_search/asteroids/views.py
+++ asteroid_search/asteroids/views.py
@@ -5,14 +5,17 @@ from rest_framework.response import Response
 from asteroids.services.query_params_service import QueryParamsService
 from asteroids.services.get_asteroids_service import GetAsteroidsService
 from asteroids.serializers import WidthHeightSerializer
+from upload_files.services.upload_data_service import UploadDataService
 from rest_framework.exceptions import ParseError
 
 query_params_service = QueryParamsService()
 get_asteroids_service = GetAsteroidsService()
+upload_data_service = UploadDataService()
 def index(request):
     return render(request, 'asteroid_form.html')
 
 def asteroids_list(request):
+    upload_data_service.check_files_not_upload()
     params = query_params_service.get_object_params(request.META['QUERY_STRING'])
     width_height_serializer = WidthHeightSerializer(data=params)
     if not width_height_serializer.is_valid(raise_exception=False):
